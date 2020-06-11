6fe39c0cc7d85d2be3969f275fa8d1e22ddfaee0
宗史 嘉陽 <wiz_reid@vivid.ocn.ne.jp>
2020-06-12 02:39:03 +0900

    新規追加

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static private string Er = "error";
+        static void Main(string[] args)
+        {
+
+            var line1 = System.Console.ReadLine().Trim();
+            if (int.TryParse(line1, out int Input) && (0 < Input && Input < 101))
+            {
+                for (int i = 1; i <= Input; i++)
+                {
+                    switch (i)
+                    {
+                        case int n when n % 5 == 0 && n % 3 == 0:
+                            Console.WriteLine("Fizz Buzz");
+                            break;
+                        case int n when n % 5 == 0:
+                            Console.WriteLine("Buzz");
+                            break;
+                        case int n when n % 3 == 0:
+                            Console.WriteLine("Fizz");
+                            break;
+                        default:
+                            Console.WriteLine(Er);
+                            break;
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine(Er);
+            }
+        }
+    } 
+}
